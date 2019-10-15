94e29d9599e869d1f9fbe1e8808ad0ff119e0ec1
Mohammad Sakib <moh4mmadsakib@gmail.com>
2019-10-15 19:45:13 +0600

    Create newotp.php

---
+++ test/newotp.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once 'src/otp.class.php';
+session_start();
+
+if (isset($_POST["email"])) {
+$otp = new OTP;
+$code = $otp->generateRandomString(6);
+$email = $_POST['email'];
+$hash = $otp->CreateOTP($email,$code);
+$_SESSION['email'] = $email;
+$_SESSION['hash'] = $hash;
+header("Location: verifyotp.php"); 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>OTP Verification without Database</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<style type="text/css">
+	.login-form {
+		width: 340px;
+    	margin: 50px auto;
+	}
+    .login-form form {
+    	margin-bottom: 15px;
+        background: #f7f7f7;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 30px;
+    }
+    .login-form h2 {
+        margin: 0 0 15px;
+    }
+    .form-control, .btn {
+        min-height: 38px;
+        border-radius: 2px;
+    }
+    .btn {        
+        font-size: 15px;
+        font-weight: bold;
+    }
+</style>
+</head>
+<body>
+<div class="login-form">
+    <form action="" method="post">
+        <h3 style="font-size: 20px;" class="text-center">OTP Verification without DB</h3>       
+        <div class="form-group">
+            <input type="email" name="email" class="form-control" placeholder="Email" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>       
